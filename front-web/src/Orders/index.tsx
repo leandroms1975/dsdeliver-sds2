43c771629a36e606648d6a9c214ce4ad8bb00857
ALEX LEANDRO MONTEIRO DOS SANTOS <leandroms1975@hotmail.com>
2021-01-10 22:32:00 -0300

    Search arder location

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -3,11 +3,13 @@ import './styles.css';
 import { ReactComponent as Logo, ReactComponent } from './logo.svg';
 import StepsHeader from './StepsHeader';
 import ProductsList from './ProductsList';
-import { Product } from './Types';
+import { OrderLocationdata, Product } from './Types';
 import { fetchProducts } from './api';
+import OrderLocation from './OrderLocation';
 
 function Orders() {
     const [products, setProducts] = useState<Product[]>([]);
+    const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
 
     useEffect(() => {
         console.log('componete orders iniciou')
@@ -21,6 +23,7 @@ function Orders() {
         <div className="orders-container">
             <StepsHeader />
             <ProductsList products={products} />
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
         </div>
     )
 }
